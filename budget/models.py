e986472db3eddbcb4de56430f85c9996e67cae42
Andy Situ <and.situ@gmail.com>
2017-08-11 21:15:46 -0700

    some model changes

--- budget/models.py
+++ budget/models.py
@@ -1,20 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class ExpenditureType(models.Model):
+    type = models.CharField(max_length=50)
+
+
 class Expenditure(models.Model):
     name = models.CharField(max_length=50, default='')
     amount_spent = models.FloatField(default=0.00)
     date_spent = models.DateTimeField('date money was spent')
 
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    type = models.ManyToManyField(ExpenditureType)
 
     def __str__(self):
         return str(self.amount_spent)
+
+class ExpenditureOverTime(Expenditure):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+class RecurringExpenditure(Expenditure):
+    frequency_day = models.IntegerField()
 
 class Income(models.Model):
     name = models.CharField(max_length=50, default='')
     amount = models.FloatField(default=0.00)
     date = models.DateTimeField('date money was earned')
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.income
