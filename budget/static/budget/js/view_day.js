472b0bd2adbed740f1f8ab2d4ab9fd9e5e878d96
Andy Situ <and.situ@gmail.com>
2017-09-16 00:35:08 -0700

    rudimentary menu option

--- budget/static/budget/js/view_day.js
+++ budget/static/budget/js/view_day.js
@@ -1,1 +1,20 @@
-console.log("HELLO");
+$( document ).ready(function() {
+    //JS for Pressing menu button
+    $('#navbarSideButton').on('click', function() {
+        $('#navbarSide').addClass('reveal');
+        $('.overlay').show();
+    });
+
+    //JS for pressing out of menu screen
+    $('.overlay').on('click', function() {
+        $('#navbarSide').removeClass('reveal');
+        $( '#menu-option-div' ).hide();
+        $('.overlay').hide()
+    });
+
+    //JS for pressing Add button
+    $('#add-button').click(function(){
+        $('.overlay').show();
+        $( '#menu-option-div' ).show();
+    });
+});
