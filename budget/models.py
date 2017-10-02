6687b2c3768798d51c1133340676494211a8dda7
Andy Situ <and.situ@gmail.com>
2017-10-01 23:07:15 -0700

    model changes

--- budget/models.py
+++ budget/models.py
@@ -3,20 +3,28 @@ from django.contrib.auth.models import User
 import datetime
 
 
+class Day(models.Model):
+    date = models.DateField()
+
+
 class BalanceAccount(models.Model):
     name = models.CharField(max_length=25)
     date = models.DateField(default=datetime.date.today)
     amount = models.FloatField()
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
+
 class ExpenditureType(models.Model):
     type = models.CharField(max_length=50)
+    description = models.CharField(max_length=200, default="")
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
 
 class Expenditure(models.Model):
     name = models.CharField(max_length=50, default='')
     amount_spent = models.FloatField(default=0.00)
-    date_spent = models.DateTimeField('date money was spent')
+    date = models.DateTimeField('date money was spent')
+    days = models.ManyToManyField(Day)
 
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     type = models.ManyToManyField(ExpenditureType)
@@ -24,22 +32,28 @@ class Expenditure(models.Model):
     def __str__(self):
         return str(self.amount_spent)
 
-class ExpenditureOverTime(Expenditure):
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
 
 class RecurringExpenditure(Expenditure):
     frequency_day = models.IntegerField()
+
 
 class Income(models.Model):
     name = models.CharField(max_length=50, default='')
     amount = models.FloatField(default=0.00)
     date = models.DateTimeField('date money was earned')
+    days = models.ManyToManyField(Day)
 
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.income
+
+
+class IncomeType(models.Model):
+    type = models.CharField(max_length=50)
+    description = models.CharField(max_length=200, default="")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
 
 class ProjectedIncome(Income):
     estimated_amount = models.FloatField()
