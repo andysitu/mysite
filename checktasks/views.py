07cf32b48ed27a6fdafa81186378dcfebaf9cd7e
Andy Situ <and.situ@gmail.com>
2018-01-20 23:20:51 -0800

    set blank templates for checktasks

--- checktasks/views.py
+++ checktasks/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 
 def view_tasks(request):
-    return HttpResponse("HELLO")
+    return render(
+        request,
+        "checktasks/view.html",
+    )
