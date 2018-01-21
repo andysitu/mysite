d8375f42d79dc08ca11568d74643254d5e81a2bc
Andy Situ <and.situ@gmail.com>
2018-01-20 23:17:07 -0800

    set basic task & date model

--- checktasks/models.py
+++ checktasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+import datetime
+
+class DateRecord(models.Model):
+    date = models.DateField()
+
+class Tasks(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    completed_dates = models.ManyToManyField(DateRecord)
