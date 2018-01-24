9efe0bb3629c1f9ec309a5bb26ac43f141b26119
Andy Situ <and.situ@gmail.com>
2018-01-23 22:50:46 -0800

    update

--- static/checktasks/js/view.js
+++ static/checktasks/js/view.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){
     $("#add-btn").click(function() {
         add_menu.create_menu();
-        $('.overlay').show();
     });
 
     $('.overlay').click(function() {
         add_menu.close_menu();
-        $('.overlay').hide()
     });
 });
 
 
 var add_menu = {
     index: {
-        name_input: "name_input",
+        name: "name",
+        name_input_element: "name_input",
+        description: "description",
+        description_textarea: "description_text",
+
     },
     create_menu: function() {
         var $menu_option_div = $( '#menu-option-div' );
@@ -23,9 +25,11 @@ var add_menu = {
         var $form = this.create_form();
         $menu_option_div.append($form);
         $( '#menu-option-div' ).show();
+        $('.overlay').show();
     },
     close_menu: function() {
         $( '#menu-option-div' ).hide();
+        $('.overlay').hide()
     },
     create_form: function() {
         var $form = $("<form>", {
@@ -36,19 +40,38 @@ var add_menu = {
 
         var $div;
 
+        // Name Input
         $div = $("<div class='form-group row'></div>");
         $("<label>", {
             "class": "col-sm-2 col-form-label",
-            "for": this.index.name_input,
+            "for": this.index.name_input_element,
         }).text("Name").appendTo($div);
 
         $("<input/>", {
-            id: this.index.name_input,
+            id: this.index.name_input_element,
             "class": "col-sm-4 form-control",
             name: "name",
         }).appendTo($div);
         $form.append($div);
 
+        // // Text Area for Description
+        // $div = $("<div class='form-group row'></div>");
+        // $("<label>", {
+        //     "for": this.index.description_textarea,
+        //     "class": "col-form-label col-sm-2",
+        //     "text": "Description",
+        // }).appendTo($div);
+        //
+        // $("<textarea>", {
+        //     "class": "form-control col-sm-8",
+        //     name: this.index.description,
+        //     id: this.index.description_textarea,
+        //     rows: 4,
+        // }).appendTo($div);
+        //
+        // $form.append($div);
+
+        // Submit Button
         $("<button/>", {
             "type": "submit",
             "class": "btn btn-primary",
@@ -58,8 +81,11 @@ var add_menu = {
         $("<button/>", {
             "class": "btn btn-secondary",
             text: "Cancel",
+            click: function(e) {
+                e.preventDefault();
+                add_menu.close_menu();
+            },
         }).appendTo($form);
-
 
         return $form;
     }
