0d8ec997926b68e3091ffa4a42478494bda591c4
Andy Situ <and.situ@gmail.com>
2018-03-06 23:17:55 -0800

    update

--- checktasks/models.py
+++ checktasks/models.py
@@ -6,8 +6,7 @@ import datetime
 class DateRecord(models.Model):
     date = models.DateField()
 
-class Tasks(models.Model):
+class Task(models.Model):
     name = models.CharField(max_length=30)
-    description = models.TextField()
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    completed_dates = models.ManyToManyField(DateRecord)
+    completed_dates = models.ManyToManyField(DateRecord, blank=True)
--- checktasks/views.py
+++ checktasks/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 
+from .models import DateRecord, Task
+
 def view_tasks(request):
+    tasks = Task.objects.all()
+    tasks_list = []
+
+    for task in tasks:
+        tasks_list.append(task)
+
+
     return render(
         request,
         "checktasks/view.html",
+        {
+            tasks: tasks_list,
+        }
     )
 
 def add(request):
-    return HttpResponse("HELLO")
+    user = request.user
+    if request.method == "POST":
+        name = request.POST.get("name")
+        task = Task(name=name, user=user)
+    return JsonResponse({"name": name})
