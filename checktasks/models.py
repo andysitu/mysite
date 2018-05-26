30b23a490460be33614e593276a9e0e63f1f196e
Andy Situ <and.situ@gmail.com>
2018-05-25 23:04:57 -0700

    make TaskBase

--- checktasks/models.py
+++ checktasks/models.py
@@ -6,7 +6,9 @@ import datetime
 class DateRecord(models.Model):
     date = models.DateField()
 
-class Task(models.Model):
+class TaskBase(models.Model):
     name = models.CharField(max_length=30)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+class Task(TaskBase):
     completed_dates = models.ManyToManyField(DateRecord, blank=True)
